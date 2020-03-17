2017955101a0e835d7efe3fd35433623fb64f0ab
Yauheni_Bulava <jonbulova97@gmail.com>
2020-03-17 17:33:26 +0300

    feat: implemented functions 'min', 'max' and 'avg'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }else{
+    var min = array[0];
+    for(var i = 0; i<=array.length-1; i++){
+      if(array[i]<min){
+        min = array[i];
+      }
+    }
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }else{
+    var max = array[0];
+  for(var i = 0; i<=array.length-1; i++){
+    if(array[i]>max){
+      max = array[i];
+    }
+  }
+  return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || array.length == 0){
+    return 0;
+  }else{
+    var middle = 0; 
+    for(var i = 0; i<=array.length-1; i++){
+      middle += array[i];
+    }
+  return middle/array.length;
+  }
 }
+
